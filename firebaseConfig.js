ac06468944f9163da9c95ca4b82b37bb76b69809
Chris Ray Belarmino <raychrisbelarmino@gmail.com>
2016-10-26 09:52:58 +0800

    Copied firebaseConfig.js from master to Chiz branch

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+var config = {
+	apiKey: "AIzaSyA_5u5aw6cGRN2Oc_gXlesX2uQd0mFkxdc",
+    authDomain: "dental-depot.firebaseapp.com",
+    databaseURL: "https://dental-depot.firebaseio.com",
+    storageBucket: "dental-depot.appspot.com",
+    messagingSenderId: "202165862730"
+};
+firebase.initializeApp(config);
