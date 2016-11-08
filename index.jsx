9d81552b3bb9cfcdd935a7aad987ff9485b13d67
robbcal <robeecal@gmail.com>
2016-11-09 07:56:41 +0800

    added notification for email address

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 
 var Content = React.createClass({
   login: function(){
+		var form = document.getElementById("emailForm").formValidation();
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if(email && password){
@@ -16,8 +17,8 @@ var Content = React.createClass({
         });
       }).catch(function(error) {
         var errorCode = error.code;
-        var errorMessage = error.message;
-        //alert(errorMessage);
+        var errorMessage = "Incorrect email and/or password";
+        alert(errorMessage);
         document.getElementById("email").value = "";
         document.getElementById("password").value = "";
       });
@@ -25,19 +26,6 @@ var Content = React.createClass({
       //alert("Input required data!");
     }
   },
-
-  // reset: function(){
-  //   var auth = firebase.auth();
-  //   var emailAddress = document.getElementById("fg_email").value;
-  //
-  //   auth.sendPasswordResetEmail(emailAddress).then(function() {
-  //     alert("Email sent.");
-  //     document.getElementById("fg_email").value = "";
-  //     $('#forgotPassModal').modal('hide');
-  //   }, function(error) {
-  //     alert(error);
-  //   });
-  // },
 
   render: function() {
     return (
@@ -56,42 +44,21 @@ var Content = React.createClass({
             </div>
           </a>
         </div>
-        <div className="login-box-body" style={{ paddingLeft: 0, paddingRight: 30, backgroundColor: '#d2d6de'}}>
-          <div className="form-group has-feedback">
-            <input type="email" id="email" className="form-control" placeholder="email address" style={{width: 476.5, height: 39, borderRadius: 7}}/>
-          </div>
-          <div className="form-group has-feedback">
-            <input type="password" id="password" className="form-control" placeholder="password" style={{width: 476.5, height: 39, borderRadius: 7}} />
-          </div>
-
-          <div className="col-lg-12 col-md-12 col-sm-12" style={{width: 432, paddingLeft: 0, paddingRight: 12}}>
-            <center><button className="btn btn-primary col-lg-12 col-md-12 col-sm-12" style={{width: 475, height: 39, borderRadius: 7}} onClick={this.login}>LOGIN</button></center>
-
-          </div><br/><br/>
-          {/* <div className="col-lg-12 col-md-12 col-sm-12" style={{ paddingTop: 10}}>
-            <center><a href="" data-toggle="modal" data-target="#forgotPassModal">I forgot my password</a></center>
-          </div> */}
-        </div>
-      </div>
-
-      {/* <div className="modal fade bs-example-modal-sm" id="forgotPassModal" tabIndex="-1" role="dialog" aria-labelledby="forgotPassModal">
-        <div className="modal-dialog modal-sm" role="document">
-        <div className="modal-content">
-        <div className="modal-header">
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 className="modal-title" id="myModalLabel">Reset Password</h4>
-        </div>
-        <div className="modal-body">
-        <input className="form-control" type="text" id="fg_email" placeholder="Email" required/><br/>
-        </div>
-        <div className="modal-footer">
-        <button type="button" className="btn btn-default pull-left" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary" onClick={this.reset}>Reset Password</button>
-        </div>
-        </div>
-        </div>
-      </div> */}
-
+				<div className="login-box-body" style={{ paddingLeft: 0, paddingRight: 30, backgroundColor: '#d2d6de'}}>
+					<form id="emailForm" className="form-horizontal" style={{width: 476.5, height: 150, marginTop: 200}}>
+						<div className="form-group has-feedback">
+							<input type="email" id="email" className="form-control" placeholder="email address" style={{width: 476.5, height: 39, borderRadius: 7}}/>
+						</div>
+						<div className="form-group has-feedback">
+							<input type="password" id="password" className="form-control" placeholder="password" style={{width: 476.5, height: 39, borderRadius: 7}} />
+						</div>
+						<div className="col-lg-12 col-md-12 col-sm-12" style={{width: 432, paddingLeft: 0, paddingRight: 12}}>
+							<center><button className="btn btn-primary col-lg-12 col-md-12 col-sm-12" style={{width: 475, height: 39, borderRadius: 7, right: 15}} onClick={this.login}>LOGIN</button></center>
+					</div>
+				</form>
+				<br/><br/>
+				</div>
+			</div>
     </div>
     );
   }
@@ -123,17 +90,16 @@ var Main = React.createClass({
     if(this.state.signedIn == false){
       res = (
         <div>
-					<div
-						<i className="fa fa-refresh fa-spin"></i>
-						</div>
-						<Content/>
+					<Content/>
         </div>
           );
-          }else{
+    }else{
       res = (
-        <div>
-          {/* <i className="fa fa-refresh fa-spin"></i> */}
-        </div>
+				<body className="hold-transition login-page">
+					<div>
+						<center><i className="fa fa-refresh fa-spin" style={{fontSize: 50}}></i></center>
+					</div>
+				</body>
       );
       if(this.state.type == "admin"){
         window.location.replace("admin/Inventory.html");
