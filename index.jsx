56d8fc0c5bc73c5fa76c6454483e018bf9733615
robbcal <robeecal@gmail.com>
2016-11-09 12:09:13 +0800

    use the new AdminLTE css

--- index.jsx
+++ index.jsx
@@ -33,10 +33,10 @@ var Content = React.createClass({
       <div className="login-box" style={{ width: 500}}>
         <div className="login-logo col-lg-12 col-md-12 col-sm-12" style={{ paddingLeft: 50, paddingRight: 10, paddingBottom: 10, marginBottom: 0}}>
           <a href="http://127.0.0.1:8080/">
-            <div className="col-lg-6 col-md-6 col-sm-6" style={{ paddingRight: 20, width: 175, paddingLeft: 30}}>
+            <div className="col-lg-6 col-md-6 col-sm-6" style={{ paddingRight: 0, width: 175, paddingLeft: 40}}>
               <img src="bootstrap/icons/tooth.png" style={{ height: 100, marginTop: 80, width: 100}}/>
             </div>
-            <div className="col-lg-6 col-md-6 col-sm-6" style={{ paddingLeft: 0, paddingRight: 110, right: 15}}>
+            <div className="col-lg-6 col-md-6 col-sm-6" style={{ paddingLeft: 0, paddingRight: 110, right: 0}}>
               <span ><h1 style={{color: '#4b6d9d', marginTop: 90, fontFamily: 'Montserrat',
                 fontWeight: 101, fontSize: 39,
                 lineHeight: 1.1,
@@ -46,14 +46,14 @@ var Content = React.createClass({
         </div>
 				<div className="login-box-body" style={{ paddingLeft: 0, paddingRight: 30, backgroundColor: '#d2d6de'}}>
 					<form id="emailForm" className="form-horizontal" style={{width: 476.5, height: 150, marginTop: 200}}>
-						<div className="form-group has-feedback">
+						<div className="form-group has-feedback" style={{marginLeft: 15}}>
 							<input required type="email" id="email" className="form-control" placeholder="email address" style={{width: 476.5, height: 39, borderRadius: 7}}/>
 						</div>
-						<div className="form-group has-feedback">
+						<div className="form-group has-feedback" style={{marginLeft: 15}}>
 							<input required type="password" id="password" className="form-control" placeholder="password" style={{width: 476.5, height: 39, borderRadius: 7}} />
 						</div>
 						<div className="col-lg-12 col-md-12 col-sm-12" style={{width: 432, paddingLeft: 0, paddingRight: 12}}>
-							<center><button className="btn btn-primary col-lg-12 col-md-12 col-sm-12" style={{width: 475, height: 39, borderRadius: 7, right: 15, marginTop: 3}} onClick={this.login}>LOGIN</button></center>
+							<center><button className="btn btn-primary col-lg-12 col-md-12 col-sm-12" style={{width: 475, height: 39, borderRadius: 7, left: 15, marginTop: 3}} onClick={this.login}>LOGIN</button></center>
 						</div>
 					</form>
 					<br/><br/>
