a562fed8e87c155d90b0200947e0079a2074371d
Chris Ray Belarmino <raychrisbelarmino@gmail.com>
2016-11-09 16:58:41 +0800

    fixed error in Release 1 ver.1

--- admin/js/Users.jsx
+++ admin/js/Users.jsx
@@ -11,10 +11,6 @@ var Header = React.createClass({
     $.AdminLTE.pushMenu.activate("[data-toggle='offcanvas']");
   },
 
-  showConfirmLogout: function(){
-    $('#confirmModal').appendTo("body").modal("show");
-  },
-
   render: function() {
     return (
         <div>
@@ -29,30 +25,9 @@ var Header = React.createClass({
                     <div className="navbar-custom-menu">
                         <ul className="nav navbar-nav">
                             <li className="dropdown user user-menu">
-                                <a href="#" className="btn btn-default" data-toggle="modal" data-target="#confirmModal" style={{borderWidth: 0, lineHeight: 0, color: "rgba(255, 255, 255, 0.15)", top: 3, right: 5}} onClick={this.showConfirmLogout}>
-                                  <span><img style={{top: 5, right: 15}} className="profileDropdown" src="../bootstrap/icons/tooth.png"/></span>
+                                <a href="#" className="dropdown-toggle profile" data-toggle="dropdown">
+                                    <span onClick={this.logout}><img className="profileDropdown" src="../bootstrap/icons/tooth.png"/></span>
                                 </a>
-                                <div className="example-modal">
-                                  <div className="modal" id="confirmModal">
-                                    <div className="modal-dialog">
-                                      <div className="modal-content">
-                                        <div className="modal-header">
-                                          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">x</span>
-                                          </button>
-                                          <h4 className="modal-title">Log-out</h4>
-                                        </div>
-                                        <div className="modal-body">
-                                          <center><p> Are you sure you want to log-out?</p></center>
-                                        </div>
-                                        <div className="modal-footer">
-                                          <button type="button" className="btn btn-default pull-left" data-dismiss="modal" aria-label="No">NO</button>
-                                          <button type="button" className="btn btn-primary" data-dismiss="modal" aria-label="No" onClick={this.logout}>YES</button>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
                             </li>
                         </ul>
                     </div>
@@ -109,11 +84,7 @@ var Content = React.createClass({
         cur_password: snapshot.val().password
       });
     });
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> Cha
     var ref = firebase.database().ref('users');
     ref.on('child_added', function(data) {
       var id=data.key
@@ -204,23 +175,6 @@ var Content = React.createClass({
     return (
       <div>
           <br/><br/>
-<<<<<<< HEAD
-          <div className="row col-lg-12 col-md-12 col-sm-12 col-xs-12">    
-            <table className="table table-hover table-striped table-bordered /*adminTable*/" id="user_table">
-              <thead>
-                <tr>
-                  <th><center>USERNAME</center></th>
-                  <th><center>EMAIL ADDRESS</center></th>
-                  <th><center>USER TYPE</center></th>
-                </tr>
-              </thead>
-              <tbody id="userList">
-                
-              </tbody>
-            </table>
-          </div>      
-        </div>
-=======
           <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
               <div className="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
                   <span className="pull-left">
@@ -242,7 +196,6 @@ var Content = React.createClass({
                           </tr>
                       </thead>
                       <tbody id="userList">
->>>>>>> Cha
 
                       </tbody>
                   </table>
