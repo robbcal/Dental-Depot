5a5ee76e18cc85b78565c4807caeacac8c03d85a
robbcal <robeecal@gmail.com>
2016-11-17 08:44:45 +0800

    modified UI of Specific User

--- admin/js/SpecificUser.jsx
+++ admin/js/SpecificUser.jsx
@@ -11,29 +11,34 @@ var Header = React.createClass({
     $.AdminLTE.pushMenu.activate("[data-toggle='offcanvas']");
   },
 
+  showConfirmLogout: function(){
+    $('#confirmModal').appendTo("body").modal("show");
+    $('[data-toggle="tooltip"]').tooltip();
+  },
+
   render: function() {
     return (
-        <div>
-            <div className="main-header">
-                <div className="logo">
-                    <span className="logo-lg" id="mainHeader">Dental Depot</span>
-                </div>
-                <div className="navbar navbar-static-top" role="navigation">
-                    <a href="#" className="sidebar-toggle" data-toggle="offcanvas" role="button">
-                        <span className="sr-only">Toggle navigation</span>
-                    </a>
-                    {/* comment */}
-                    <div className="navbar-custom-menu">
-                        <ul className="nav navbar-nav">
-                            <li className="dropdown user user-menu">
-                                <a href="#"><span onClick={this.logout}>
-                                    <img className="profileDropdown" src="../bootstrap/icons/tooth.png" data-toggle="tooltip" title="Logout" data-placement="bottom"/>
+        <div className="wrapper">
+          <header className="main-header">
+            <a href="Inventory.html" className="logo">
+              <span className="logo-mini"><b>DD</b></span>
+              <span className="logo-lg" id="mainHeader">Dental Depot</span>
+            </a>
+            <nav className="navbar navbar-static-top">
+              <a href="#" className="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span className="sr-only">Toggle navigation</span>
+              </a>
+              <div className="navbar-custom-menu">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="#"><span onClick={this.logout}>
+                                    <img className="profileDropdown" src="../bootstrap/icons/tooth.png" data-toggle="tooltip" title="Logout" data-placement="left"/>
                                 </span></a>
                             </li>
                         </ul>
                     </div>
-                </div>
-            </div>
+                </nav>
+            </header>
         </div>
     );
   }
@@ -41,34 +46,28 @@ var Header = React.createClass({
 
 var Body = React.createClass({
   render: function() {
-      return (
+    return (
         <div>
-          <div className="main-sidebar">
-            <div className="sidebar">
+          <aside className="main-sidebar">
+            <section className="sidebar">
               <ul className="sidebar-menu">
                 <br/>
                 <li className="header">NAVIGATION</li>
-                <li className="active"><a href="Inventory.html"><i><img src="../bootstrap/icons/boxes.png" id="sidebarImage"/></i><span id="sidebarMainTabs">Inventory</span></a></li>
-                <li><a href="Users.html"><i><img src="../bootstrap/icons/multiple-users-silhouette.png" id="sidebarImage"/></i><span id="sidebarMainTabs">Users</span></a></li>
-                <li><a href="SpecificUser.html"><i><img src="../bootstrap/icons/graph-line-screen.png" id="sidebarImage"/></i><span id="sidebarMainTabs">Specific User</span></a></li>
-                <li><a href="AdminProfile.html"><i className="fa fa-user" id="sidebarImage"></i><span id="sidebarProfileTab">Profile</span></a></li>
+                <li><a href="Inventory.html"><i className="fa fa-archive" id="sidebarImage"></i><span>Inventory</span></a></li>
+                <li><a href="Users.html"><i className="fa fa-users" id="sidebarImage"></i><span>Users</span></a></li>
+                <li><a href="Logs.html"><i className="fa fa-line-chart" id="sidebarImage"></i><span>Logs</span></a></li>
+                <li className="active"><a href="Profile.html"><i className="fa fa-user" id="sidebarImage"></i><span>Profile</span></a></li>
               </ul>
+            </section>
+          </aside>
+          <div className="content-wrapper">
+            <section id="content" className="content" style={{backgroundColor: "rgb(236, 240, 245)", width: 930, marginLeft: 60}}><Content/></section>
             </div>
-          </div>
-
-          <div style={{height: '588px', backgroundColor: '#e1e1e1'}}>
-            <div className="content-wrapper" style={{height: '588px', backgroundColor: '#e1e1e1', paddingLeft: 80, paddingRight: 30, paddingTop: 10}}>
-              <div id="content" className="content" style={{backgroundColor: '#e1e1e1'}}>
-                <Content/>
-              </div>
-            </div>
-          </div>
-
-          {/* LOGOUT MODAL CONTENT */}
         </div>
     );
   }
 });
+
 
 var Content = React.createClass({
   getInitialState: function() {
@@ -102,6 +101,24 @@ var Content = React.createClass({
         password: snapshot.val().password
       });
     });
+  },
+
+  checkProfile: function(){
+      var firstname = document.getElementById("firstName").value;
+      var lastname = document.getElementById("lastName").value;
+      var address = document.getElementById("address").value;
+      var contactnumber = document.getElementById("contactNumber").value;
+      var email = document.getElementById("email").value;
+      var age = document.getElementById("age").value;
+      var birthdate = document.getElementById("birthdate").value;
+      var password = document.getElementById("password").value;
+
+      if(firstname != "" && lastname != "" && address != "" && contactnumber != "" && email != "" && age != "" && birthdate != "" && password != ""){
+          $('#editConfirmation').appendTo("body").modal('show');
+      }else{
+          document.getElementById("errorMessage").innerHTML= "Missing input.";
+          $('#errorModal').appendTo("body").modal('show');
+      }
   },
 
   showModal: function(){
@@ -182,25 +199,46 @@ var Content = React.createClass({
     }
   },
 
+  deleteUser: function(){
+    var now = new Date();
+    var today = now.getFullYear()+"-"+(now.getMonth()+1)+"-"+now.getDate();
+    var uid = firebase.auth().currentUser.uid;
+    var action = "Deleted user."
+
+    // firebase.database().ref("users/"+uid+"/activity").push().set({
+    //   action: action,
+    //   itemID: itemID,
+    //   // itemName: this.state.userName,
+    //   quantity: "n/a",
+    //   date: today
+    // });
+
+    firebase.database().ref('users/'+userID).remove();
+    alert(action);
+    window.location.replace("Users.html");
+  },
+
+
   render: function() {
       return (
         <div id="mainContent">
 
-          <div className="nav-tabs-custom" style={{paddingLeft: 0}}>
-
-            <ul className="nav nav-tabs pull-right">
+          <div className="nav-tabs-custom" style={{paddingLeft: 0, width: 920}}>
+            <ul className="nav nav-tabs pull-right" style={{width: 920}}>
               <a href="Inventory.html" className="pull-left"><img src="../bootstrap/icons/left-arrow.png" height="25px" style={{paddingLeft:  15, paddingTop: 10, width: 40, height: 35}}/></a>
               <li><a href="#activity" data-toggle="tab">ACTIVITY</a></li>
               <li className="active"><a href="#userProfileMainContent" data-toggle="tab">PROFILE</a></li>
             </ul>
 
-            <div className="tab-content" style={{width: 470, height: 400}}>
+            <div className="tab-content table-responsive" style={{width: 920, height:420}}>
+
               <div className="tab-pane" id="activity">
                 <div className="row">
-                  <div className="col-sm-6 pull-right">
+                  <div className="col-sm-8"></div>
+                  <div className="col-sm-4 pull-right">
                     <div className="box-tools pull-right">
-                      <div className="input-group input-group-sm" id="logsTransSearch">
-                        <input type="text" name="table_search" className="form-control pull-right" placeholder="Search"/>
+                      <div className="input-group input-group-md" id="logsTransSearch">
+                        <input type="text" name="tableSearch" className="form-control pull-right" placeholder="Search" id="logsSearch"/>
                         <div className="input-group-btn">
                           <button type="submit" className="btn btn-default">
                             <i className="fa fa-search"></i>
@@ -210,72 +248,29 @@ var Content = React.createClass({
                     </div>
                   </div>
                 </div>
-                <div className="row">
+                <div className="table-responsive">
                   <div className="col-sm-12">
+                    <br/>
                     <div className="box-body">
-                      <table id="example1" className="table table-bordered table-striped striped dataTable">
+                      <table id="example1" className="table table-bordered table-hover dataTable">
                         <thead>
                           <tr>
-                            <th><center>TRANSACTION ID</center></th>
-                            <th><center>TOTAL</center></th>
+                            <th><center>ACTION</center></th>
+                            <th><center>ITEM/PERSON</center></th>
                             <th><center>DATE</center></th>
-                            <th><center>USER</center></th>
+                            
                           </tr>
                         </thead>
-                        <tbody>
-                          <tr>
-                            <td>Trident</td>
-                            <td>Win 95+</td>
-                            <td> 4</td>
-                            <td>X</td>
-                          </tr>
-                          <tr>
-                            <td>Trident</td>
-                            <td>Win 95+</td>
-                            <td> 4</td>
-                            <td>X</td>
-                          </tr>
-                          <tr>
-                            <td>Trident</td>
-                            <td>Win 95+</td>
-                            <td> 4</td>
-                            <td>X</td>
-                          </tr>
+                        <tbody id="activityList">
+
                         </tbody>
                       </table>
                     </div>
                   </div>
                 </div>
-                <div className="row" id="logsRowThree">
-                  <div className="col-sm-5">
-                    <div className="dataTables_info" id="example2_info" role="status" aria-live="polite">
-                      Showing 1 to 10 of 57 entries
-                    </div>
-                  </div>
-                  <div className="col-sm-7">
-                    <div className="dataTables_paginate paging_simple_numbers pull-right" id="logsTransPagination">
-                      <ul className="pagination">
-                        <li className="paginate_button previous disabled">
-                          <a href="#" aria-controls="example2" data-dt-idx="0" tabindex="0">Previous</a>
-                        </li>
-                        <li className="paginate_button active">
-                          <a href="#" aria-controls="example2" data-dt-idx="1" tabindex="0">1</a>
-                        </li>
-                        <li className="paginate_button">
-                          <a href="#" aria-controls="example2" data-dt-idx="2" tabindex="0">2</a>
-                        </li>
-                        <li className="paginate_button next">
-                          <a href="#" aria-controls="example2" data-dt-idx="2" tabindex="0">Next</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
               </div>
 
-              <div className="active tab-pane" id="userProfileMainContent" style={{height: 444, marginLeft: 0, width: 900}}>
-                {/* <div className="row col-xs-8 box" style={{paddingLeft: 20}}>
-                <div> */}
+              <div className="active tab-pane" id="userProfileMainContent" style={{height: 360, marginLeft: 0, width: 890}}>
                 <h2><strong style={{paddingLeft: 50}}> {this.state.fullName} </strong></h2>
                 <h4 style={{paddingLeft: 50}}> {this.state.address} </h4><br/>
                 <div className="row" style={{paddingLeft: 50}}>
@@ -310,27 +305,24 @@ var Content = React.createClass({
                     <h5 style={{color: 'gray'}}><strong> CONTACT NUMBER </strong></h5>
                     <h4><strong> {this.state.contactNumber} </strong></h4><br/>
                   </div>
-                  {/* </div>
-                  </div> */}
                 </div>
 
                 <div className="row" id="userProfileButtons">
                   <div className="col-sm-6"></div>
-                  <div className="col-sm-6 pull-right" style={{paddingRight: 0, paddingTop: 20, paddingLeft: 270}}>
+                  <div className="col-sm-6 pull-right" style={{paddingRight: 0, paddingTop: 25, paddingLeft: 280, marginRight: 60}}>
                     <a className="btn btn-primary pull-right" id="editInfoButton" data-toggle="modal" data-target="#editInfoModal" style={{width: 80}}
                       onClick={this.showModal}>EDIT
                     </a>
                     <div className="col-sm-6">
-                      <a className="btn btn-primary pull-right" id="editInfoButton" data-toggle="modal" data-target="#editInfoModal"
-                        onClick={this.showModal}>DELETE
-                      </a>
+                      <a className="btn btn-primary pull-right" id="deleteUserButton" data-toggle="modal" data-target="#deleteUserModal">DELETE</a>
                     </div>
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
-          {/* </div> */}
+
 
           {/*MODAL CONTENT*/}
 
@@ -345,7 +337,26 @@ var Content = React.createClass({
                       <button type="button" className="btn btn-default" data-dismiss="modal" id="confirmProfileEdit">NO</button>
                     </center>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
+          <div className="example-modal">
+            <div className="modal fade bs-example-modal-lg" id="deleteUserModal">
+              <div className="modal-dialog modal-md">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 className="modal-title">Delete User</h4>
+                  </div>
+                  <div className="modal-body col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <center><h4><strong>Are you sure you want to delete this user?</strong></h4></center>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-default pull-left" data-dismiss="modal">NO</button>
+                    <button type="button" className="btn btn-primary" onClick={this.deleteUser}>YES</button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -417,7 +428,7 @@ var Content = React.createClass({
                       </div>
                     </div>
                     <div className="row">
-                      <div id="editInfoModalComponents">
+                      <div id="editInfoModalComponents" style={{marginLeft: 15, marginRight: 15}}>
                         <label>Address</label>
                         <input type="text" id="address" className="form-control" onChange={this.onAddress} value={this.state.address}/>
                       </div>
@@ -441,8 +452,7 @@ var Content = React.createClass({
                   </div>
                   <div className="modal-footer">
                     <button type="button" className="btn btn-default pull-left" data-dismiss="modal">CANCEL</button>
-                    <button type="button" className="btn btn-primary" id="editConfirmBtn" data-toggle="modal" data-target="#editConfirmation"
-                      onClick={this.showConfirmationModal}>SAVE</button>
+                    <button type="button" className="btn btn-primary" id="editConfirmBtn" onClick={this.checkProfile}>SAVE</button>
                   </div>
                 </div>
               </div>
